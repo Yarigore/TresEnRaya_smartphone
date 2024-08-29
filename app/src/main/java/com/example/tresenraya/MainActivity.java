e6e4e0c3ff1dade92b2e47d936d7e2600159f962
Dimas <63927493+Yarigore@users.noreply.github.com>
2024-08-30 00:02:49 +0200

    Creación de interfaz básica

--- app/src/main/java/com/example/tresenraya/MainActivity.java
+++ app/src/main/java/com/example/tresenraya/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.tresenraya;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // INICIAMOS EL ARRAY CASILLAS QUE IDENTIFICA CADA CASILLA Y LA ALMACENA EN EL ARRAY
+        casillas = new int[9];
+        casillas[0] = R.id.a1;
+        casillas[1] = R.id.a2;
+        casillas[2] = R.id.a3;
+        casillas[3] = R.id.b1;
+        casillas[4] = R.id.b2;
+        casillas[5] = R.id.b3;
+        casillas[6] = R.id.c1;
+        casillas[7] = R.id.c2;
+        casillas[8] = R.id.c3;
     }
+
+    public void Jugar(View view){
+
+        ImageView imagen;
+
+        for (int casilla : casillas){
+            imagen = findViewById(casilla);
+            imagen.setImageResource(R.drawable.casilla);
+        }
+    }
+
+    private int jugadores;
+
+    private int[] casillas;
+    private Partida partida;
 }
